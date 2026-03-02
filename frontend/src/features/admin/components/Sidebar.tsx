724afcdeec8583b40f404d395d3698192c9e6d9d
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-02 17:25:46 +0800

    logout functionality

--- frontend/src/features/admin/components/Sidebar.tsx
+++ frontend/src/features/admin/components/Sidebar.tsx
@@ -1,7 +1,22 @@
-import { Link, NavLink } from "react-router";
+import { Link, NavLink, useNavigate } from "react-router";
 import { MdOutlineGroupAdd } from "react-icons/md";
+import { useMutation } from "@tanstack/react-query";
+import { logoutAdmin } from "../admin.service";
+import { useAuthenticatedAdmin } from "@/hooks/useAuthenticatedAdmin";
+import { RiLogoutBoxLine } from "react-icons/ri";
 
 export default function Sidebar() {
+	const navigate = useNavigate();
+	const { clearAdmin } = useAuthenticatedAdmin();
+
+	const { mutate: logout, isPending } = useMutation({
+		mutationFn: logoutAdmin,
+		onSuccess: () => {
+			clearAdmin();
+			navigate("/admin/login", { replace: true });
+		},
+	});
+
 	return (
 		<aside className="w-96 hidden md:flex flex-col border-r border-zinc-200">
 			<nav className="px-6 py-4 h-full flex flex-col gap-2 w-full items-center">
@@ -10,30 +25,59 @@ export default function Sidebar() {
 						MILE 365 RUN CLUB
 					</h1>
 				</Link>
-				<ul className="w-full">
-					<li className="flex flex-col gap-2">
-						<p className="text-xs text-zinc-500">Club Management</p>
-						<ul>
-							<li>
-								<NavLink
-									to="membership-requests"
-									className={({ isActive }) =>
-										`flex items-center gap-2 px-4 py-2 rounded-md transition ${
-											isActive
-												? "bg-zinc-100 font-normal"
-												: "hover:bg-zinc-100"
-										}`
-									}
-								>
-									<MdOutlineGroupAdd className="text-xl" />
-									<div className="text-sm">
-										Membership Requests
-									</div>
-								</NavLink>
-							</li>
-						</ul>
-					</li>
-				</ul>
+
+				<div className="w-full flex flex-col gap-8">
+
+					{/* CLUB MANAGEMENT */}
+					<ul className="w-full">
+						<li className="flex flex-col gap-2">
+							<p className="text-xs text-zinc-500">
+								Club Management
+							</p>
+							<ul>
+								<li>
+									<NavLink
+										to="membership-requests"
+										className={({ isActive }) =>
+											`flex items-center gap-2 px-4 py-2 rounded-md transition ${
+												isActive
+													? "bg-zinc-100 font-normal"
+													: "hover:bg-zinc-100"
+											}`
+										}
+									>
+										<MdOutlineGroupAdd className="text-xl" />
+										<div className="text-sm">
+											Membership Requests
+										</div>
+									</NavLink>
+								</li>
+							</ul>
+						</li>
+					</ul>
+
+					{/* ACCOUNT CONTROLS */}
+					<ul className="w-full">
+						<li className="flex flex-col gap-2">
+							<p className="text-xs text-zinc-500">Account</p>
+							<ul>
+								<li>
+									<button
+										onClick={() => logout()}
+										disabled={isPending}
+										className="w-full text-sm gap-2 px-4 py-2 flex items-center hover:bg-zinc-100"
+									>
+										<RiLogoutBoxLine className="text-xl" />
+										{isPending
+											? "Logging out..."
+											: "Logout"}
+									</button>
+								</li>
+							</ul>
+						</li>
+					</ul>
+
+				</div>
 			</nav>
 		</aside>
 	);
