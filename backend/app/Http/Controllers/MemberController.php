7ec54e6dafb1144cc6a24fc9cd7ed2693baec80d
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-04 17:20:20 +0800

    minor update

--- backend/app/Http/Controllers/MemberController.php
+++ backend/app/Http/Controllers/MemberController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\MemberResource;
 use App\Models\Member;
+use App\Services\MemberService;
 use Illuminate\Http\Request;
 
 class MemberController extends Controller
 {
+    protected MemberService $memberService;
+
+    public function __construct(MemberService $memberService)
+    {
+        $this->memberService = $memberService;
+    }
+
     public function index()
     {
-        $members = Member::get();
+        $members = $this->memberService->getAllMembers();
+
         return MemberResource::collection($members);
     }
 }
