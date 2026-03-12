643e4522c10461079a3969be04754f3d11450bc6
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-12 22:19:08 +0800

    join form csrf

--- frontend/src/features/membership/membership.service.ts
+++ frontend/src/features/membership/membership.service.ts
@@ -1,7 +1,8 @@
-import { api } from "@/lib/api";
+import { api, sanctum } from "@/lib/api";
 import type { MembershipRequestInput } from "@/features/membership/types/membership-request.types";
 
 export async function submitMembershipRequest(payload: MembershipRequestInput) {
+	await sanctum.get("/sanctum/csrf-cookie");
 	const response = await api.post("membership-requests", payload);
 	return response.data;
 }
