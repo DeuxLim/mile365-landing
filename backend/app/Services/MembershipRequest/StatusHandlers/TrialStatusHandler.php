c9cf04d46028dd7dcab71ea3310f1ace12ff6e32
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-16 17:34:13 +0800

    trial status handler

---
+++ backend/app/Services/MembershipRequest/StatusHandlers/TrialStatusHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services\MembershipRequest\StatusHandlers;
+
+use App\Events\MembershipRequestTrial;
+use App\Models\MembershipRequest;
+use InvalidStatusTransitionException;
+use StatusHandlerInterface;
+
+class TrialStatusHandler implements StatusHandlerInterface
+{
+    public function validate(MembershipRequest $membershipRequest, array $inputs)
+    {
+        if (!$membershipRequest->status === "pending") {
+            return throw new InvalidStatusTransitionException("Status cannot be approved if previous status is not 'pending'");
+        }
+    }
+
+    public function handle(MembershipRequest $membershipRequest, array $inputs)
+    {
+        event(new MembershipRequestTrial($membershipRequest));
+    }
+}
