d785fa5014fe60a21b914a90b511f74ec754165c
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-17 16:25:48 +0800

    super admin and club admin provision

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Auth\AuthenticationException;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withCommands()
     ->withRouting(
         api: [
             __DIR__ . '/../routes/api.php',
---
+++ backend/config/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'super_admin' => [
+        'email' => env('SUPER_ADMIN_EMAIL'),
+        'first_name' => env('SUPER_ADMIN_FIRST_NAME'),
+        'last_name' => env('SUPER_ADMIN_LAST_NAME'),
+        'password' => env('SUPER_ADMIN_PASSWORD'),
+    ],
+];
+
