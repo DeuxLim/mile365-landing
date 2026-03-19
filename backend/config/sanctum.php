0c8a0d0a1172b4e4c4f6ad546dd3f1bde729c4b4
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-19 20:49:31 +0800

    spa to token auth migration : sanctum config update

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -34,7 +34,7 @@ return [
     |
     */
 
-    'guard' => ['web'],
+    'guard' => [],
 
     /*
     |--------------------------------------------------------------------------
@@ -47,7 +47,7 @@ return [
     |
     */
 
-    'expiration' => null,
+    'expiration' => 120,
 
     /*
     |--------------------------------------------------------------------------
@@ -75,10 +75,6 @@ return [
     |
     */
 
-    'middleware' => [
-        'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
-        'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
-        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
-    ],
+    'middleware' => [],
 
 ];
