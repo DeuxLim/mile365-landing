9fa33b9573cf8c0101ccf6bb6ed02b761a4e562e
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-20 09:32:42 +0800

    Remove backend (moved to separate repository)

--- backend/app/Services/MembershipRequest/StatusHandlers/InvalidStatusTransitionException.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace App\Services\MembershipRequest\StatusHandlers;
-
-class InvalidStatusTransitionException extends \RuntimeException {}
--- backend/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\MembershipRequestController;
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\MemberController;
